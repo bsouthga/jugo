64c92e4e8ef5085a47383e1831db35f92779595f
Ben Southgate <bsouthga@gmail.com>
2015-03-14 16:20:36 -0400

    aggregation

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 
 var database = require('./lib/database.js'),
     monitor = require('./lib/monitor.js'),
-    collect = require('./lib/collect.js'),
     universe = require('./lib/universe.js');
 
 
 function jugo(config) {
 
+  var db = database(config);
+
   function collect() {
-    // open database...
-    database.open(function() {
+    // open database connection...
+    db.open(function() {
       // get authorities and their followers...
       universe.populate(config.accounts, function(auth_data) {
         // stream tweets from authorities and followers...
         monitor(auth_data).tweet(function(tweet){
           // each time there is a tweet, log and collect
           console.log('@'+tweet.user.screen_name + ':::' + tweet.text);
-          collect(tweet);
+          // add tweet to database
+          db.add(tweet);
         })
         // begin collecting
         .init();
@@ -24,14 +26,27 @@ function jugo(config) {
     });
   }
 
+  function get(query, callback) {
+    query = query || {};
+    db.open(function() {
+      db.get(query, callback)
+    })
+  }
+
   return {
-    collect : collect
+    collect : collect,
+    get : get
   }
 
 }
 
-var jugo_config = require('./lib/jugo.json');
 
-jugo().collect(jugo_config)
+var jugo_config = require('./jugo.json');
+
+var J = jugo(jugo_config);
+
+J.get({ max_date : new Date() }, function(results) {
+    console.log(results)
+  });
 
 //module.exports = jugo;
